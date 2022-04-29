c33d64949b368f20851911e752b2047ba3c9be31
BrianDJardine <104565860+BrianDJardine@users.noreply.github.com>
2022-04-29 14:58:08 +0100

    Update streamlit_app.py
    Move the function up

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page 
 streamlit.dataframe(fruits_to_show)
 
+# create function 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normailized = pandas.json_normalize(fruityvice_response.json())
+    
+    return fruityvice_normailized
+
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -37,16 +45,7 @@ try:
 
 except URLError as e:
   streamlit.error()
-
-# create function 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    # take the json version of the response and normalize it
-    fruityvice_normailized = pandas.json_normalize(fruityvice_response.json())
-    
-    return fruityvice_normailized
   
-
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
